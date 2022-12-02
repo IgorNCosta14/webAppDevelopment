8010eb370840fbecb12de076fc432b68641c0311
Igor Nascimento Costa <igorncosta14@hotmail.com>
2022-12-02 15:54:00 -0300

    example6

---
+++ example6/app.js
@@ -0,0 +1,17 @@
+var main = function (toDoObjectss) {
+    "use strict";
+    var toDos = toDoObjects.map(function (toDo) {
+    // we'll just return the description
+    // of this toDoObject
+    return toDo.description;
+    });
+    // now all of our old code should work exactly as it did!
+// ...
+};
+
+$(document).ready(function () {
+$.getJSON("todos.json", function (toDoObjects) {
+// we'll call main with toDos as an argument
+main(toDoObjects);
+});
+});
